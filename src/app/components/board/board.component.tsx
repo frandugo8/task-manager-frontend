ae240bbcef41b25f325e61468d64a76629bfd693
Fran <franciscojesus.dugo@e-campus.uab.cat>
2022-03-18 19:19:28 +0100

    Changed TableComponent to BoardComponent

---
+++ src/app/components/board/board.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './board.module.scss'
+
+export default function BoardComponent() {
+  return (
+    <div className={styles.board}>
+    </div>
+  )
+}
--- src/app/components/table/table.component.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './table.module.scss'
-
-export default function TableComponent() {
-  return (
-    <div className={styles.table}>
-    </div>
-  )
-}
